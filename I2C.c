85b01f2bd602464c610d278ff2671b7f81881d6a
Ahmed F. El-Adawy <AhmedFathi77@users.noreply.github.com>
2018-09-04 03:09:41 +0200

    Update I2C.c

--- I2C.c
+++ I2C.c
@@ -1,49 +1,35 @@
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/delay.h>
-
-void I2C_Init(){
+#include "I2C.h"
+void I2C_Init (){
 	TWSR = 0x00;		// 0 PreScaler
-	TWBR = 0x0C; 		// 400 kbit/s moode
-	TWCR |= (1<<TWEN);	// Enable T2C
+	TWAR = 0b00000010;
+	TWBR = 0x02; 		// 400 kbit/s moode
+	TWCR |= (1<<TWEN); // Enable T2C
+
 }
 void I2C_Start(){
-	 TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
-	while((TWCR &(1<<TWINT))==0);
-
+	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+	while(BIT_IS_CLEAR(TWCR,TWINT));
 }
-void I2C_End(){
-	 TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
+void I2C_Stop(){
+	TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
 }
-void I2C_SendData(unsigned char Data){
-	TWDR = Data;
-
-	 TWCR = (1<<TWINT)|(1<<TWEN);
-	while((TWCR & (1<<TWINT)) == 0){}
+unsigned char I2C_ReadACK(){
+		TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+	    while(BIT_IS_CLEAR(TWCR,TWINT));
+	    return TWDR;
 }
-unsigned char I2C_ReadData_With_AK(){
-	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
-	// Wait until TWINT flag set to make sure Start Bit is send successfully
-	while((TWCR & (1<<TWINT)) == 0);
-	return TWDR;
-}
-unsigned char I2C_ReadData_With_NonAK(){
+unsigned char I2C_ReadNACK(){
 	TWCR = (1<<TWINT)|(1<<TWEN);
-	while((TWCR & (1<<TWINT)) == 0);
+	while(BIT_IS_CLEAR(TWCR,TWINT));
 	return TWDR;
 }
-
-unsigned char TWI_Get_Status(void)
-{
-	unsigned char status;
-    // masking to eliminate first 3 bits and get the last 5 bits (status bits)
-    status = TWSR & 0xF8;
-    return status;
+void I2C_Write(unsigned char data){
+	 TWDR = data;
+	 TWCR = (1 << TWINT) | (1 << TWEN);
+	 while(BIT_IS_CLEAR(TWCR,TWINT));
 }
-void I2C_Slave_init(unsigned char Address){
-	TWAR = Address;
-	TWCR |= (1<<TWEN);	// Enable T2C
-	TWCR |=(1<<TWINT);	// Clear TWINT flag before sending the start bit
-	TWCR |=(1<<TWEA);	// Enable Acknowledgement
+unsigned char I2C_GetStatus(){
+	unsigned char status;
+	status = TWSR & 0xF8;
+	return status;
 }
