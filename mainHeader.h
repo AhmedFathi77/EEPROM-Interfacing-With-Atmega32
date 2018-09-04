c2fb65725bea8932b7af1d4dd0a60d362fd2be9d
Ahmed F. El-Adawy <AhmedFathi77@users.noreply.github.com>
2018-09-04 03:13:21 +0200

    Add files via upload

---
+++ mainHeader.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: Sep 2, 2018
+ *      Author: leenovoz510
+ */
+
+#ifndef MAINHEADER_H_
+#define MAINHEADER_H_
+#define BIT_IS_CLEAR(REG,BIT) ( !(REG & (1<<BIT)) )
+#include <avr/io.h>
+#include <avr/delay.h>
+
+
+#endif /* MAINHEADER_H_ */
